38fd2ddfad64f3ce7f5e6964300faa653e5f583f
Faraaz Khan <faraaz@rationalizeit.us>
2012-01-02 16:02:28 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PodCastGenerator::Application.config.secret_token = '84d71041d7991251779e5e1b385c779620ae033c6c9aa1049636c0315b253472693d4e7ff4aa726859230fc7bc307a541a7d5e5e853b0a609c0236d6c5068c34'
